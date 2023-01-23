6b289b5ba5e543012237e84fa4d74146671fed99
maxsainsbury <maxwell.sainsbury@gmail.com>
2023-01-23 00:13:26 -0800

    Linked TrackList.js to Playlist.js

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './Playlist.css';
+import { TrackList } from "../TrackList/TrackList";
 
 export class Playlist extends React.Component {
 
@@ -8,7 +9,7 @@ export class Playlist extends React.Component {
     return (
       <div className="Playlist">
         <input defaultValue={'New Playlist'} />
-        //TrackList
+        <TrackList />
         <button className="Playlist-save">SAVE TO SPOTIFY</button>
       </div>
     );
